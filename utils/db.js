0c6b5e1857876c436034034c782a9928ddad84b8
micoliser <micoliser@gmail.com>
2023-10-03 17:38:19 +0100

    Mandatory done

---
+++ utils/db.js
@@ -0,0 +1,129 @@
+/* eslint-disable comma-dangle */
+import crypto from 'crypto';
+import { MongoClient, ObjectId } from 'mongodb';
+
+async function createConnection() {
+  const host = process.env.DB_HOST ? process.env.DB_HOST : 'localhost';
+  const port = process.env.DB_PORT ? process.env.DB_PORT : 27017;
+  const database = process.env.DB_DATABASE
+    ? process.env.DB_DATABASE
+    : 'files_manager';
+  const url = `mongodb://${host}:${port}`;
+
+  const client = new MongoClient(url, { useUnifiedTopology: true });
+  await client.connect();
+  const db = client.db(database);
+  return db;
+}
+
+class DBClient {
+  constructor() {
+    this.db = null;
+    createConnection().then((db) => {
+      this.db = db;
+      this.users = db.collection('users');
+      this.files = db.collection('files');
+    });
+    this.sHash = crypto.createHash('sha1');
+  }
+
+  isAlive() {
+    if (this.db) return true;
+    return false;
+  }
+
+  async nbUsers() {
+    return this.users.countDocuments();
+  }
+
+  async nbFiles() {
+    return this.files.countDocuments();
+  }
+
+  async createUser(email, password) {
+    const existingUser = await this.users.findOne({ email });
+    if (existingUser) throw Error('User already exist');
+    this.sHash.update(password);
+    const hashedPassword = this.sHash.digest('hex');
+    const result = await this.users.insertOne({
+      email,
+      password: hashedPassword,
+    });
+    return result.insertedId;
+  }
+
+  async getUser(id) {
+    if (!id) return null;
+    const user = await this.users.findOne({ _id: ObjectId(id) });
+    return user;
+  }
+
+  async verifyUser(email, password) {
+    if (!email || !password) return null;
+    const user = await this.users.findOne({ email });
+    if (!user) return null;
+    this.sHash.update(password);
+    const hashedPassword = this.sHash.digest('hex');
+    if (hashedPassword !== user.password) return null;
+    return user;
+  }
+
+  async createFile(name, type, parentId, isPublic, userId, localPath) {
+    const result = await this.files.insertOne({
+      name,
+      type,
+      parentId,
+      userId,
+      isPublic,
+      localPath,
+    });
+    return result.insertedId;
+  }
+
+  async createFolder(name, type, parentId, isPublic, userId) {
+    const result = await this.files.insertOne({
+      name,
+      type,
+      parentId,
+      userId,
+      isPublic,
+    });
+    return result.insertedId;
+  }
+
+  async getFile(id) {
+    if (!id) return null;
+    const file = await this.files.findOne({ _id: ObjectId(id) });
+    return file;
+  }
+
+  async verifyFileUser(fileId, userId) {
+    if (!fileId || !userId) return null;
+    const file = await this.getFile(fileId);
+    if (!file) return null;
+    if (file.userId.toString() !== userId.toString()) return null;
+    return file;
+  }
+
+  async getFiles(parentId, page = 0) {
+    const pipeline = [{ $skip: Number(page) * 20 }, { $limit: 20 }];
+    // eslint-disable-next-line eqeqeq
+    if (parentId && parentId != 0) {
+      pipeline.unshift({ $match: { parentId } });
+    }
+    const files = await this.files.aggregate(pipeline).toArray();
+    return files;
+  }
+
+  async updateFile(id, update) {
+    if (!id || !update) return null;
+    const result = await this.files.updateOne(
+      { _id: ObjectId(id) },
+      { $set: update }
+    );
+    return result;
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
---
+++ worker.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-await-in-loop */
+import Bull from 'bull';
+import fs from 'fs/promises';
+import imageThumbnail from 'image-thumbnail';
+import dbClient from './utils/db';
+
+const fileQueue = new Bull('fileQueue');
+
+fileQueue.process(async (job) => {
+  const { userId, fileId } = job.data;
+  if (!fileId) throw new Error('Missing fileId');
+  if (!userId) throw new Error('Missing userId');
+  const file = await dbClient.verifyFileUser(fileId, userId);
+  if (!file) throw new Error('File not found');
+  console.log('Processing new job:');
+  for (const width of [100, 250, 500]) {
+    const thumbnail = imageThumbnail(file.localPath, {
+      width,
+    });
+    console.log('Creating thumbnail');
+    const thumbnailName = `${file.localPath}_${width}`;
+    console.log(`Created thumbnail in ${thumbnailName}`);
+    await fs.writeFile(thumbnailName, thumbnail);
+  }
+});
